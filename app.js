047cae29136cd3ffcfc9b755fb2e358ae108d404
quangthao2002 <quangthao2212002@gmail.com>
2024-09-17 06:43:36 +0700

    init project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./configs/db.js");
+
+// Load biến môi trường từ file .env
+dotenv.config();
+
+// Kết nối với MongoDB
+connectDB();
+
+const app = express();
+app.use(express.json()); // Để xử lý dữ liệu JSON
+
+// Ví dụ route đơn giản
+app.get("/", (req, res) => {
+  res.send("IUH TSE Club API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
