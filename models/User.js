d16ca37be606f8a785e69403292eee085a05ff2d
quangthao2002 <quangthao2212002@gmail.com>
2024-09-17 07:31:02 +0700

    Encry password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -14,12 +15,29 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  phone: {
+    type: String,
+    required: true,
+  },
   role: {
     type: String,
     enum: ["admin", "member", "visitor"],
     default: "visitor",
   },
 });
+
+// Mã hóa mật khẩu trước khi lưu vào DB
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
